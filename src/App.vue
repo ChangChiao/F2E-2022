0d7122610c5f81848fab3c36eca2d08374be4322
joe <joe.chang1014@gmail.com>
2022-11-11 10:55:52 +0800

    feat resize reload

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,32 @@
 import Index from "./views/Index.vue";
 import { onMounted } from "vue";
 let windowWidth = 0;
+
+const throttle = (callback, wait) => {
+  console.log("callback, wait", callback, wait);
+  let timeStamp = 0;
+  return function (...args) {
+    if (Date.now() - timeStamp > wait) {
+      callback.apply(this, args);
+      timeStamp = Date.now();
+    }
+  };
+};
+
 const checkDevice = () => {
+  console.warn("windowWidth", windowWidth);
+  console.warn(" window.innerWidth", window.innerWidth);
   if (
-    windowWidth <= 1024 &&
-    window.innerHeight > 1024 &&
-    windowWidth >= 1024 &&
-    window.innerHeight < 1024
+    (windowWidth <= 1024 && window.innerWidth > 1024) ||
+    (windowWidth >= 1024 && window.innerWidth < 1024)
   ) {
-    alert("請重新整理！確保動畫可以正常運行");
+    location.reload();
+    // alert("請重新整理！確保動畫可以正常運行");
   }
 };
 onMounted(() => {
   windowWidth = window.innerWidth;
-  window.onresize = checkDevice;
+  window.addEventListener("resize", throttle(checkDevice, 500));
   window.onbeforeunload = function () {
     window.scrollTo(0, 0);
   };
